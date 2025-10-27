7503bc9f1b1d2d8efdc3f753de8d11b425dd84c1
misatomobe <marimekko.0219@gmail.com>
2025-10-27 21:07:10 +0900

    script

--- js/script.js
+++ js/script.js
@@ -12,12 +12,16 @@ new Splide( '.splide',{
 
 // ヘッダー背景色
 const header = document.querySelector('.header');
+const about = document.querySelector('#about');
 
 header.style.transition = 'background-color 0.4s';
 
 window.addEventListener('scroll', () => {
+	const aboutPosition = about.offsetTop; // ← ★ ABOUTセクションの位置を取得
+	const scrollY = window.scrollY;
+	
 	if (window.scrollY > 0) {
-		header.style.backgroundColor = 'rgba(0, 0, 0, 0.7)';
+		header.style.backgroundColor = 'rgba(77, 150, 0, 0.7)';
 		header.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.2)';
 	} else {
 		header.style.backgroundColor = 'transparent';
