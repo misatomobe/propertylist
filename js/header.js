baa6dc9684c39caa48e872acc99ea465ceeea5e6
misatomobe <marimekko.0219@gmail.com>
2025-11-02 10:16:21 +0900

    header

--- js/header.js
+++ js/header.js
@@ -7,3 +7,16 @@ hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('active');
   spNav.classList.toggle('active');
 });
+
+// ヘッダーの背景変更
+window.addEventListener('scroll', function () {
+  const header = document.querySelector('.header');
+  const aboutSection = document.querySelector('#about');
+  const aboutTop = aboutSection.offsetTop;
+
+  if (window.scrollY >= aboutTop - 50) {
+    header.classList.add('is-colored');
+  } else {
+    header.classList.remove('is-colored');
+  }
+});
