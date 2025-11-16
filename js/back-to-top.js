5b5fc32d5ab400190f7dfffd4df538f280c94212
misatomobe <marimekko.0219@gmail.com>
2025-11-16 16:50:55 +0900

    go-to-topボタン

---
+++ js/back-to-top.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const backToTop = document.getElementById('back-to-top');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+      backToTop.classList.add('show');
+    } else {
+      backToTop.classList.remove('show');
+    }
+  });
+
+  backToTop.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
+});
