ebd33a22c5611907f6017ca88adbd528955d6775
misatomobe <marimekko.0219@gmail.com>
2026-01-25 21:08:05 +0900

    works

--- js/works-modal.js
+++ js/works-modal.js
@@ -8,11 +8,19 @@ document.addEventListener('DOMContentLoaded', () => {
   // aタグ全体をクリック対象にする
   const modalLinks = document.querySelectorAll('.works-item__link');
 
+  let scrollpos = 0;
+
   modalLinks.forEach((link) => {
     link.addEventListener('click', (e) => {
       e.preventDefault(); // ← ページ遷移を防ぐ
 
       const img = link.querySelector('.js-modal-image'); // 内部の画像を取得
+
+      // 固定背景
+      scrollPos = window.scrollY;
+      document.body.classList.add('is-fixed');
+      document.body.style.top = `-${scrollPos}px`;
+
       modal.classList.add('is-active');
       modalImg.src = img.dataset.modal;
       modalImg.alt = img.alt;
@@ -24,6 +32,12 @@ document.addEventListener('DOMContentLoaded', () => {
   [modalClose, overlay].forEach((el) => {
     el.addEventListener('click', () => {
       modal.classList.remove('is-active');
+
+      // 背景固定解除
+      document.body.classList.remove('is-fixed');
+      document.body.style.top = '';
+      window.scrollTo(0, scrollPos);
+
       modalImg.src = '';
       modalCaption.textContent = '';
     });
