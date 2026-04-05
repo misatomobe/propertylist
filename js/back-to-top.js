1bb9b372026d06246eac9c33ced03d5eca02dfb8
misatomobe <marimekko.0219@gmail.com>
2026-04-05 22:18:31 +0900

    back-to-top

--- js/back-to-top.js
+++ js/back-to-top.js
@@ -1,39 +1,25 @@
-const topBtn = document.querySelector('.top-back-btn'); // ボタン
-const fv = document.querySelector('.mv'); // FVセクション
-const modal = document.querySelector('.modal'); // モーダル
+const topBtn = document.querySelector('.top-back-btn');
+const footer = document.querySelector('footer');
+const mv = document.querySelector('.mv');
+const modal = document.querySelector('.modal');
 
-// ボタン表示判定関数
 function updateTopBtn() {
   const scrollY = window.scrollY;
-  const fvHeight = fv.offsetHeight;
-  const pageHeight = document.documentElement.scrollHeight;
-  const windowHeight = window.innerHeight;
+  const fvHeight = window.innerHeight;
+  const footerTop = footer.offsetTop;
 
   if (modal.classList.contains('is-active')) {
-    // モーダル開いてる → 非表示
     topBtn.classList.remove('show');
-    topBtn.classList.add('hide');
-  } else if (scrollY > fvHeight && scrollY < pageHeight - windowHeight) {
-    // FV下かつページ下部でない → 表示
+    return;
+  }
+
+  // FVより下 ＆ footerより上
+  if (scrollY > fvHeight && scrollY < footerTop -50) {
     topBtn.classList.add('show');
-    topBtn.classList.remove('hide');
   } else {
-    // それ以外 → 非表示
     topBtn.classList.remove('show');
-    topBtn.classList.add('hide');
   }
 }
 
-// スクロール時に判定
 window.addEventListener('scroll', updateTopBtn);
-
-// モーダル開閉時に判定
-function openModal() {
-  modal.classList.add('is-active');
-  updateTopBtn(); // ここで即時判定
-}
-
-function closeModal() {
-  modal.classList.remove('is-active');
-  updateTopBtn(); // ここで即時判定
-}
+window.addEventListener('load', updateTopBtn);
