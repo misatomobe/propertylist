6135e1c08336c33a5c55ce06b04a9329420e3076
misatomobe <marimekko.0219@gmail.com>
2026-04-05 20:16:09 +0900

    fade-up

--- js/fade-up.js
+++ js/fade-up.js
@@ -1,11 +1,16 @@
-const elements = document.querySelectorAll('.fade-up');
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll('.fade-up');
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('is-active');
-    }
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('is-active');
+      }
+    });
+  }, {
+    threshold: 0.1,
+    rootMargin: "0px 0px -40% 0px"
   });
-});
 
-elements.forEach(el => observer.observe(el));
+  elements.forEach(el => observer.observe(el));
+});
