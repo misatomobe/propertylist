e8241c388fed10a4284f71ff618d6231272c983c
misatomobe <marimekko.0219@gmail.com>
2026-05-05 11:29:13 +0900

    back_to_btn

--- js/script.js
+++ js/script.js
@@ -142,7 +142,7 @@ function updateTopBtn() {
   let triggerOffset = 100; // SP・タブレット
 
   if (window.innerWidth >= 1024) {
-    triggerOffset = 150; // PC
+    triggerOffset = 200; // PC
   }
 
   if (
